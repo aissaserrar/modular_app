6a904b127029292c851c1246b81918c67d97b116
Aissa Serrar <serrar@zencode.io>
2025-07-11 17:24:26 +0100

    Init dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
+import 'package:modular_cinema/di/injection.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  setupDI(Environment.dev);
   runApp(const MyApp());
 }
 
@@ -39,18 +43,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
 
         title: Text(widget.title),
       ),
       body: Center(
-
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
